e97fae796dac196840c6a86cbfa691fbdc510b51
Venkat Peri <venkatperi@gmail.com>
2017-08-10 12:21:57 -0400

    travis2

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
-const gulp = require('gulp');
-const gulpDocumentation = require('gulp-documentation');
-const mocha = require('gulp-mocha');
-const jshint = require('gulp-jshint')
-const istanbul = require('gulp-istanbul')
-const npmcheck = require('gulp-npm-check');
-const path = require('path');
-const arrayp = require('arrayp');
-const promisify = require('util').promisify;
-const exec = promisify(require('child_process')
-  .exec);
-require('gulp-release-it')(gulp);
+const gulp = require( 'gulp' );
+const gulpDocumentation = require( 'gulp-documentation' );
+const mocha = require( 'gulp-mocha' );
+const jshint = require( 'gulp-jshint' )
+const istanbul = require( 'gulp-istanbul' )
+const npmcheck = require( 'gulp-npm-check' );
+const path = require( 'path' );
+const arrayp = require( 'arrayp' );
+const promisify = require( 'util' ).promisify;
+const exec = promisify( require( 'child_process' ).exec );
+require( 'gulp-release-it' )( gulp );
 
 const tests = ['simple', 'base', 'vendor', 'multi-env', 'fullSchema'];
 
@@ -19,38 +18,38 @@ const srcDirs = {
   doc: "doc"
 }
 
-gulp.task('pre-test', () =>
-  arrayp.chain(tests.map((t) =>
-    exec('npm install', { cwd: path.join(__dirname, `test/${t}`) })
-    .then((res) => console.log(`test/${t}\n`, res.stdout))
-  ))
+gulp.task( 'pre-test', () =>
+  arrayp.chain( tests.map( ( t ) =>
+    exec( 'npm install', { cwd: path.join( __dirname, `test/${t}` ) } )
+      .then( ( res ) => console.log( `test/${t}\n`, res.stdout ) )
+  ) )
 )
 
-gulp.task('deps', function (cb) {
-  npmcheck(cb);
-});
+gulp.task( 'deps', function ( cb ) {
+  npmcheck( cb );
+} );
 
-gulp.task('docs', function () {
-  return gulp.src(srcDirs.js)
-    .pipe(gulpDocumentation('md'))
-    .pipe(gulp.dest('doc/'));
-});
+gulp.task( 'docs', function () {
+  return gulp.src( srcDirs.js )
+    .pipe( gulpDocumentation( 'md' ) )
+    .pipe( gulp.dest( 'doc/' ) );
+} );
 
-gulp.task('lint', function () {
-  return gulp.src(srcDirs.js)
-    .pipe(jshint({ esversion: 6, asi: true }))
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
-});
+gulp.task( 'lint', function () {
+  return gulp.src( srcDirs.js )
+    .pipe( jshint( { esversion: 6, asi: true } ) )
+    .pipe( jshint.reporter( 'default' ) )
+    .pipe( jshint.reporter( 'fail' ) );
+} );
 
-gulp.task('test', ['pre-test'], () =>
-  gulp.src(srcDirs.test, { read: false })
-  .pipe(mocha())
+gulp.task( 'test', ['pre-test'], () =>
+    gulp.src( srcDirs.test, { read: false } )
+      .pipe( mocha() )
   //.pipe( istanbul.writeReports() )
 );
 
-gulp.task('coverage', () =>
-  gulp.src(srcDirs.js)
-  .pipe(istanbul())
-  .pipe(istanbul.hookRequire())
+gulp.task( 'coverage', () =>
+  gulp.src( srcDirs.js )
+    .pipe( istanbul() )
+    .pipe( istanbul.hookRequire() )
 );
